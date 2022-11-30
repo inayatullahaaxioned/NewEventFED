8013b27b5208d88426263848f7d345df11924260
inayatullahaaxioned <inayatullaha@axioned.com>
2022-11-30 17:09:27 +0530

    hamburger and header sticky js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,27 @@
 /* Author: 
 Inayatullah
 */
+//grabbing elements
+const header = document.querySelector('.header'),
+    hamburger = document.querySelector('.hamburger'),
+    navbar = document.querySelector('.navbar');
+
+//header sticky js start
+window.addEventListener("scroll", function () {
+    if (window.scrollY > header.offsetHeight + 50) {
+        header.classList.add('scroll');
+    } else {
+        header.classList.remove('scroll');
+    }
+});
+//header sticky js end
+
+//hamburger js start
+hamburger.addEventListener('click', function () {
+    navbar.classList.toggle('active');
+    hamburger.classList.toggle('active');
+});
+//hamburger js end
 
 //speaker section responsive slider start here 
 $('.speakers-list').slick({
@@ -9,38 +30,38 @@ $('.speakers-list').slick({
     arrows: false,
     speed: 300,
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 2,
     responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4,
-          infinite: true,
-          dots: true
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 995,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+        },
+        {
+            breakpoint: 770,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
         }
-      },
-      {
-        breakpoint: 995,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
     ]
-  });
+});
 //speaker section responsive slider end here
