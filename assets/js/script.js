cbf2d587e41b0a58a2df25c57f1deb9eec1e6233
inayatullahaaxioned <inayatullaha@axioned.com>
2022-11-30 17:32:48 +0530

    tab filter in program section and accordain in faq section js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,3 +65,61 @@ $('.speakers-list').slick({
     ]
 });
 //speaker section responsive slider end here
+
+//tab fiter in program section start
+const tabs = document.querySelectorAll('.tab-item'),
+    programs = document.querySelectorAll('.program-item');
+
+//when page loads day one program become active
+tabs[0].classList.add('active');    
+programs.forEach(function (program) {
+    let checkDayNo = program.getAttribute('data-program');
+    if (checkDayNo == 'one') {
+        program.classList.add('active');
+    };
+});
+
+tabs.forEach(function (tab) {
+    tab.addEventListener('click', function () {
+        let dayNo = tab.getAttribute('data-tab');
+        showProgram(tab, dayNo);
+    })
+});
+
+//showing program list on tab fitler
+function showProgram(tab, dayNo) {
+    let activeTab = document.querySelector('.tab-item.active');
+    if (activeTab) {
+        activeTab.classList.remove('active');
+    };
+    tab.classList.add('active');
+
+    for (let list of programs) {
+        if (list.classList.contains('active')) {
+            list.classList.remove('active');
+        }
+        if (list.getAttribute('data-program') == dayNo) {
+            list.classList.add('active');
+        };
+    };
+};
+//tab fiter in program section end
+
+//accordian js start
+const question = document.querySelectorAll('.question'),
+    faqList = document.querySelectorAll('.faq-item');
+faqList[0].classList.add('active');
+question.forEach(function(faq, index) {
+    faq.addEventListener('click', function(){
+        const activeFaq = document.querySelector('.faq-item.active');
+        if(activeFaq){
+            activeFaq.classList.remove('active');
+        };
+        if(faqList[index].classList.contains('active')){
+            faqList[index].classList.remove('active')
+        }else{
+            faqList[index].classList.add('active');
+        }
+    });
+});
+//accordian js end
