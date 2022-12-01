c18ce6c4f7ddd841dc4a09656a00dacafade2a6f
inayatullahaaxioned <inayatullaha@axioned.com>
2022-12-01 11:15:11 +0530

    modal js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,8 @@ Inayatullah
 const header = document.querySelector('.header'),
     hamburger = document.querySelector('.hamburger'),
     navbar = document.querySelector('.navbar'),
-    toTopBtn = document.querySelector('.to-top-btn');
+    toTopBtn = document.querySelector('.to-top-btn'),
+    html = document.querySelector('html');
 
 
 //header sticky js start and to-top-btn showing 
@@ -24,6 +25,7 @@ window.addEventListener("scroll", function () {
 hamburger.addEventListener('click', function () {
     navbar.classList.toggle('active');
     hamburger.classList.toggle('active');
+    html.classList.toggle('scroll-lock');
 });
 //hamburger js end
 
@@ -50,7 +52,7 @@ $('.speakers-list').slick({
             breakpoint: 1024,
             settings: {
                 slidesToShow: 4,
-                slidesToScroll: 4,
+                slidesToScroll: 1,
                 infinite: true,
                 dots: true
             }
@@ -59,14 +61,14 @@ $('.speakers-list').slick({
             breakpoint: 995,
             settings: {
                 slidesToShow: 3,
-                slidesToScroll: 3
+                slidesToScroll: 1
             }
         },
         {
             breakpoint: 770,
             settings: {
                 slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToScroll: 1
             }
         },
         {
@@ -137,3 +139,29 @@ question.forEach(function (faq, index) {
     });
 });
 //accordian js end
+
+//watch video section modal start
+const watchVideoSection = document.querySelector('.watch-video'),
+    video = document.querySelector('.video figure');
+
+video.addEventListener('click', function () {
+    const modalContainer = document.createElement('div');
+    modalContainer.className = "modal-container";
+    modalContainer.innerHTML = `<div class="modal-content">
+            <span class="modal-close">Close Btn</span>
+            <video class="modal-video" poster="assets/images/intro-bg.jpg" controls autoplay>
+              <source src="assets/images/watchvideo.mp4" type="video/mp4">
+            </video>
+          </div>`;
+          watchVideoSection.appendChild(modalContainer);
+          html.classList.add('scroll-lock');
+
+          modalContainer.addEventListener('click', function(e){
+            if(!e.target.classList.contains('modal-video')){
+                modalContainer.remove();
+                html.classList.remove('scroll-lock');
+            };
+          });
+          
+});
+//watch video section modal end
